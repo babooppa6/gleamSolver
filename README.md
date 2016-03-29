bd458c0e912994acc22e86a3ef2d8b893a9c1aa9
Citrinate <phil4387@gmail.com>
2016-03-29 18:38:58 -0400

    added raffle mode, UI, and more entry types

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 #Gleam.solver
 ## Purpose ##
-Automates the process of forced participation in social media activities at Gleam.io, and then cleans up after itself.
+Automates the process of forced participation in social media activities at Gleam.io, and when possible, cleans up after itself.
 #Installation
 1. Install the [Tampermonkey extension](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en) (for Chrome) or the [Greasemonkey extension](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) (for Firefox)
 2. Go [here](https://raw.githubusercontent.com/Citrinate/gleamSolver/master/gleamSolver.user.js)
--- gleamSolver.user.js
+++ gleamSolver.user.js
@@ -1,80 +1,144 @@
 // ==UserScript==
 // @name Gleam.solver
 // @namespace https://github.com/Citrinate/gleamSolver
-// @description Autocompletes Gleam.io entries and undoes any forced social media actions
+// @description Auto-completes Gleam.io contest entries
 // @author Citrinate
-// @version 1.1
+// @version 1.2
 // @match *://gleam.io/*
 // @match https://steamcommunity.com/app/329630
 // @updateURL https://raw.githubusercontent.com/Citrinate/gleamSolver/master/gleamSolver.user.js
 // @downloadURL https://raw.githubusercontent.com/Citrinate/gleamSolver/master/gleamSolver.user.js
+// @require https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
 // @run-at document-end
 // ==/UserScript==
 
 (function() {
+	// command_hub_url is the only page on steamcommunity that this script will be injected at (as referenced in @match above)
+	// it can be any page on steamcommunity.com that can be loaded into an iframe
+	var command_hub_url = "https://steamcommunity.com/app/329630";
+	var current_version = "1.2";
+
 	var gleamSolver = (function() {
 		var gleam = null;
-		var waitingInterval = null;
-		var steamHandler = null;
-
-		function waitForGleam() {
-			if(document.querySelector(".popup-blocks-container") !== null) {
-				clearInterval(waitingInterval);
-				gleam = angular.element(document.querySelector(".popup-blocks-container")).scope();
-				handleEntries();
+		var steam_handler = null;
+		var script_mode = null;
+	
+		// possible modes:
+		// "undo_all" (Instant-win mode): There should be no record of any social media activity on the user's accounts
+		// "undo_some" (Not presently used): Only when possible, remove all record of social media activity on the user's accounts
+		// "undo_none (Raffle mode): All record of social media activity should remain on the user's accounts 
+		function determineMode() {
+			switch(gleam.campaign.campaign_type) {			
+				case "Reward": return "undo_all"; // Instant-win
+				case "Competition": return "undo_none";	// Raffle
+				default: return "undo_all";
 			}
 		}
-
+		
 		function handleEntries() {
-			var entries = document.querySelectorAll(".entry-method");
+			var entries = jQuery(".entry-method");
 
 			for(var i = 0; i < entries.length; i++) {
 				var current_entry = angular.element(entries[i]).scope();
-				
-				// reveal hidden entries
-				current_entry.entry_method.mandatory = true;
 
 				if(gleam.canEnter(current_entry.entry_method) && !gleam.isEntered(current_entry.entry_method)) {
+					//TODO: add more entry types					
 					switch(current_entry.entry_method.entry_type) {
 						case "steam_join_group":
-							if(steamHandler === null) steamHandler = loadSteamHandler.getInstance();
-							steamHandler.handleEntry(current_entry);
+							if(steam_handler === null) steam_handler = loadSteamHandler.getInstance();
+							steam_handler.handleEntry(current_entry);
 							break;
-
-						case "custom_action":
+							
 						case "youtube_subscribe":
+						case "facebook_visit":
+						case "twitchtv_enter":
+						case "twitchtv_follow":
+						case "twitter_enter":
+						case "steam_enter":
+						case "steam_play_game":
 							handleClickEntry(current_entry);
 							break;
 							
 						case "youtube_watch":
 							handleYoutubeVideoEntry(current_entry);
 							break;
-
-						//TODO: handle more entry types
+						
 						default: 
 							break;
+					}
+					
+					// for the following entries, it's not possible to automate them without 
+					// potentially being disqualified in a gleam raffle.  only handle these if the 
+					// user doesn't care about the status of the entry after this script completes:
+					// such as in the case of gleam instant-win giveaways
+					if(script_mode != "undo_none") {
+						switch(current_entry.entry_method.entry_type) {
+							case "custom_action":
+								// custom_action entries can take many different forms
+								switch(current_entry.entry_method.method_type) {
+									case "None":
+										handleClickEntry(current_entry);
+										break;
+									
+									default:
+										break;
+								}
+								break;
+							
+							default: 
+								break;
+						}
+					}
+					
+					// the following entry types cannot be undone, and so only automate them
+					// if the user doesn't want social media actions to be undone:
+					// such as in the case of gleam raffles
+					if(script_mode != "undo_all") {
+						switch(current_entry.entry_method.entry_type) {
+							case "twitter_follow":
+							case "twitter_tweet":
+							case "twitter_retweet":
+							case "email_subscribe":
+								handleClickEntry(current_entry);
+								break;
+								
+							default:
+								break;
+						}
 					}
 				}
 			}
 		}
 
-		function markEntryCompleted(entry) {
+		function markEntryCompleted(entry, callback) {
 			entry.entry_method.entering = false;
 			entry.enterLinkClick(entry.entry_method);
 			entry.verifyEntryMethod();
+			
+			// callback after gleam marks the entry as completed
+			if(typeof(callback) == "function") {
+				var temp_interval = setInterval(function() {
+					if(gleam.isEntered(entry.entry_method)) {
+						clearInterval(temp_interval);
+						callback();
+					}
+				}, 500);
+			}
 		}
 		
-		// provides visual feedback to the user that something is happening
 		function markEntryLoading(entry) {
+			// provides visual feedback to the user that something is happening
 			entry.entry_method.entering = true;
 		}
 
+		// trick gleam into thinking we've clicked a link
 		function handleClickEntry(entry) {
 			markEntryLoading(entry);
 			entry.triggerVisit(entry.entry_method.id);
 			markEntryCompleted(entry);
 		}
 
+		// trick gleam into thinking we've watched a video
 		function handleYoutubeVideoEntry(entry) {
 			markEntryLoading(entry);
 			entry.entry_method.watched = true;
@@ -91,7 +155,7 @@
 				// interface to joining and leaving Steam groups.
 				var command_hub = document.createElement('iframe');
 				command_hub.style.display = "none";
-				command_hub.src = "https://steamcommunity.com/app/329630";
+				command_hub.src = command_hub_url;
 				document.body.appendChild(command_hub);
 
 				function handleGroup(entry, group_name, group_id) {
@@ -103,9 +167,13 @@
 						// wait for a response
 						window.addEventListener("message", function(event) {
 							if(event.source == command_hub.contentWindow && event.data.id == group_id && event.data.status == "joined") {
-								// we're in the group, mark the entry and get out of there
-								markEntryCompleted(entry);
-								command_hub.contentWindow.postMessage({action: "leave", name: group_name, id: group_id}, "*");
+								// we're in the group, mark the entry
+								markEntryCompleted(entry, function() {
+									if(script_mode != "undo_none") {
+										// depending on user's choice of mode, leave the group
+										command_hub.contentWindow.postMessage({action: "leave", name: group_name, id: group_id}, "*");
+									}
+								});
 							}
 						}, false);
 					});
@@ -129,30 +197,70 @@
 		})();
 
 		return {
+			initGleam: function() {
+				// wait for gleam to finish loading
+				temp_interval = setInterval(function() {
+					if(jQuery(".popup-blocks-container") !== null) {
+						clearInterval(temp_interval);
+						gleam = angular.element(jQuery(".popup-blocks-container")).scope();
+						script_mode = determineMode();
+						var entries = jQuery(".entry-method");
+						
+						// reveal hidden entries
+						for(var i = 0; i < entries.length; i++) {
+							angular.element(entries[i]).scope().entry_method.mandatory = true;
+						}
+						
+						gleamSolverUI.loadUI();
+					}
+				}, 500);
+			},
+			
 			completeEntries: function() {
-				if(gleam === null && waitingInterval === null) {
-					// wait for gleam to finish loading
-					waitingInterval = setInterval(waitForGleam, 500);
-				} else {
-					handleEntries();
-				}
+				handleEntries();
 			}
 		};
 	})();
 
+	var gleamSolverUI = (function() {
+		var button_class = "btn btn-embossed btn-info";
+		var button_style = { margin: "2px 0px 2px 16px" };
+		var container_style = { background: "#000", padding: "8px", color: "#3498db", font: "18px Arial" };
+		var gleam_solver_ui = 
+			jQuery("<div>", { css: container_style }).append(
+				jQuery("<span>", { text: "Gleam.solver v" + current_version })).append(
+				jQuery("<a>", { text: "Click here to auto-complete", class: button_class, css: button_style}).click(function() { 
+					gleam_solver_ui.slideUp();
+					gleamSolver.completeEntries();
+				})
+			);
+	
+		return {
+			loadUI: function() {				
+				jQuery("html").prepend(gleam_solver_ui);
+			}
+		};
+	})();	
+
 	// does the actual steam group joining/leaving
-	function initCommandHub() {
-		// make note of what groups we're already a member of, so that we don't leave any of them
+	function initSteamCommandHub() {
 		var active_groups = null;
+		
+		// make note of what groups we're already a member of, so that we don't leave any of them
 		jQuery.ajax({
 			url: "https://steamcommunity.com/my/groups",
 			async: false,
 			complete: function(data) {
-				jQuery(data.responseText).find(".groupBlock a.linkTitle").each(function() {
-					var group_name = jQuery(this).attr("href").replace("https://steamcommunity.com/groups/", "");
-					if(active_groups === null) active_groups = [];
-					active_groups.push(group_name);
-				});
+				if(data.responseText.toLowerCase().indexOf("you belong to 0 groups") != -1) {
+					// user isn't a member of any steam groups
+					active_groups = [];
+				} else {
+					jQuery(data.responseText).find(".groupBlock a.linkTitle").each(function() {
+						var group_name = jQuery(this).attr("href").replace("https://steamcommunity.com/groups/", "");
+						if(active_groups === null) active_groups = [];
+						active_groups.push(group_name);
+					});
+				}
 			}
 		});
 
@@ -200,9 +308,8 @@
 
 	// determine where we are and call the appropriate function
 	if(document.location.hostname == "gleam.io") {
-		gleamSolver.completeEntries();
-	} else if(document.location.hostname == "steamcommunity.com") {
-		console.log("hello");
-		initCommandHub();
+		gleamSolver.initGleam();
+	} else if(document.location == command_hub_url) {
+		initSteamCommandHub();
 	}
 })();
